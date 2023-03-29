135528e8c1231e3a6d38b2a6a1525d45d69fc174
kkotto <ksen.sharupich.work@gmail.com>
2023-03-29 19:43:52 +0300

    number recognising added
    Took 31 seconds

--- src/main/java/com/kkotto/Constant.java
+++ src/main/java/com/kkotto/Constant.java
@@ -1,2 +1,15 @@
-package com.kkotto;public class Constants {
+package com.kkotto;
+
+public class Constant {
+    public static final int FIRST_NUMBER_INPUT_POSITION = 0;
+    public static final int ACTION_INPUT_POSITION = 1;
+    public static final int SECOND_NUMBER_INPUT_POSITION = 2;
+    public static final String INPUT_SPLIT_REGEX = " ";
+    public static final int INCORRECT_VALUE = -1;
+    public static final int DEFAULT_VALUE = 0;
+    public static final String NOT_NUMBER_EXCEPTION_TEXT = "Некорректные данные: нечисловое значение.";
+    public static final String DIFFERENT_TYPES_EXCEPTION_TEXT =
+            "Некорректные данные: используются разные системы счисления";
+    public static final String INCORRECT_ROMAN_VALUE_EXCEPTION_TEXT =
+            "Некорректные данные: данное значение не является римским числом от 1 до 10";
 }
--- src/main/java/com/kkotto/Main.java
+++ src/main/java/com/kkotto/Main.java
@@ -1,5 +1,54 @@
-package com.kkotto;public class Main {
+package com.kkotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+        calc(input);
+    }
+
+    public static String calc(String input) {
+        List<String> inputValues = Arrays.asList(input.split(Constant.INPUT_SPLIT_REGEX));
+        List<Integer> numbers = recognizeOneTypeNumbers(List.of(inputValues.get(Constant.FIRST_NUMBER_INPUT_POSITION),
+                inputValues.get(Constant.SECOND_NUMBER_INPUT_POSITION)));
+        String action = inputValues.get(1);
+        System.out.println(numbers.get(0) + " " + action + " " + numbers.get(1));
+        return null;
+    }
+
+    public static boolean isRoman(String value) {
+        return Arrays.stream(RomanNumber.values()).anyMatch(romanNumber -> romanNumber.getRomanValue().equals(value));
+    }
+
+    public static List<Integer> recognizeOneTypeNumbers(List<String> values) {
+        String firstValue = values.get(0);
+        String secondValue = values.get(1);
+        int firstNumber = Constant.DEFAULT_VALUE, secondNumber = Constant.DEFAULT_VALUE;
+        try {
+            if (isRoman(firstValue) && isRoman(secondValue)) {
+                firstNumber = RomanNumber.convertToArabic(firstValue);
+                secondNumber = RomanNumber.convertToArabic(secondValue);
+            }
+            if (isRoman(firstValue) || isRoman(secondValue)) {
+                throw new IllegalArgumentException();
+            } else {
+                firstNumber = Integer.parseInt(firstValue);
+                secondNumber = Integer.parseInt(secondValue);
+            }
+        } catch (NumberFormatException exception) {
+            LOGGER.log(Level.WARNING, Constant.NOT_NUMBER_EXCEPTION_TEXT);
+        } catch (IllegalArgumentException exception) {
+            LOGGER.log(Level.WARNING, Constant.DIFFERENT_TYPES_EXCEPTION_TEXT);
+        }
+        return List.of(firstNumber, secondNumber);
     }
 }
