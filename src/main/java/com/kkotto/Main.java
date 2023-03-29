1b1ca1416b92ae6fa435969680a403663ff577ef
kkotto <ksen.sharupich.work@gmail.com>
2023-03-29 20:30:45 +0300

    input check added
    Took 45 minutes

--- src/main/java/com/kkotto/Main.java
+++ src/main/java/com/kkotto/Main.java
@@ -13,42 +13,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         scanner.close();
-        calc(input);
+        try {
+            calc(input);
+        } catch (IncorrectDataException exception) {
+            LOGGER.log(Level.WARNING, exception.getMessage());
+        }
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws IncorrectDataException {
         List<String> inputValues = Arrays.asList(input.split(Constant.INPUT_SPLIT_REGEX));
-        List<Integer> numbers = recognizeOneTypeNumbers(List.of(inputValues.get(Constant.FIRST_NUMBER_INPUT_POSITION),
-                inputValues.get(Constant.SECOND_NUMBER_INPUT_POSITION)));
-        String action = inputValues.get(1);
-        System.out.println(numbers.get(0) + " " + action + " " + numbers.get(1));
+        if (!isInputValid(inputValues.size())) {
+            throw new IncorrectDataException(Constant.INCORRECT_INPUT_FORMAT);
+        }
         return null;
     }
 
-    public static boolean isRoman(String value) {
-        return Arrays.stream(RomanNumber.values()).anyMatch(romanNumber -> romanNumber.getRomanValue().equals(value));
-    }
-
-    public static List<Integer> recognizeOneTypeNumbers(List<String> values) {
-        String firstValue = values.get(0);
-        String secondValue = values.get(1);
-        int firstNumber = Constant.DEFAULT_VALUE, secondNumber = Constant.DEFAULT_VALUE;
-        try {
-            if (isRoman(firstValue) && isRoman(secondValue)) {
-                firstNumber = RomanNumber.convertToArabic(firstValue);
-                secondNumber = RomanNumber.convertToArabic(secondValue);
-            }
-            if (isRoman(firstValue) || isRoman(secondValue)) {
-                throw new IllegalArgumentException();
-            } else {
-                firstNumber = Integer.parseInt(firstValue);
-                secondNumber = Integer.parseInt(secondValue);
-            }
-        } catch (NumberFormatException exception) {
-            LOGGER.log(Level.WARNING, Constant.NOT_NUMBER_EXCEPTION_TEXT);
-        } catch (IllegalArgumentException exception) {
-            LOGGER.log(Level.WARNING, Constant.DIFFERENT_TYPES_EXCEPTION_TEXT);
-        }
-        return List.of(firstNumber, secondNumber);
+    public static boolean isInputValid(int inputValuesAmount) {
+        return inputValuesAmount == Constant.VALUES_AMOUNT;
     }
 }
